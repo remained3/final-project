c2376fb7842386fb355d2206658f9b654a284b51
Alex Thon <ghaiquol@gmail.com>
2022-01-14 10:53:48 -0500

    initial commits

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
+// ------------------ REQUIREMENTS -----------------
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
+// -------------- SERVER SETTINGS ---------------------
+
+var app = express();
+const PORT = 8080;
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ------------------ ROUTES/ENDPOINTS ------------------
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`final app is listening on port ${PORT}`)
+})
 
 module.exports = app;
