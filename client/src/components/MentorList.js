8ab08d70cef474e2fe515994235cff0514c56351
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-17 13:31:42 -0500

    Initial components

---
+++ client/src/components/MentorList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MentorListItem from "./MentorListItem";
+
+const MentorList = () => {
+  return <MentorListItem />;
+};
+
+export default MentorList;
---
+++ client/src/components/StudentListItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentListItem = () => {
+  return <div></div>;
+};
+
+export default StudentListItem;
