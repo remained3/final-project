38a35abd0da1d4ad7ab80be4a0dd6e325b149d5c
Alex Thon <ghaiquol@gmail.com>
2022-01-17 19:47:10 -0500

    test btn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,18 @@
 // import logo from './logo.svg';
 
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+
 import Button from '@mui/material/Button';
 
 // css
 import "./App.scss";
 
 function App() {
-  return <Button variant="contained">Example button</Button>;
+  return <Button 
+        style={{backgroundColor:"#748FFF"}}
+        variant="contained">
+        Example button
+        </Button>;
 }
 
 export default App;
