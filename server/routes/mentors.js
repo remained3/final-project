9dc95831e77a4e1207fd20fa3ebe311898f462a5
remained3 <sarahthebush@hotmail.com>
2022-01-18 18:53:21 -0500

    updated profile and db

--- server/routes/mentors.js
+++ server/routes/mentors.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET mentors listing. */
+const mentorsRouter = (db) => {
+  router.get('/',function(req, res, next) {
+    const queryString = "SELECT * FROM users WHERE mentor = true;";
+    return db
+    .query(queryString)
+    .query(queryString)
+    .then((data) => res.json(data.rows))
+    .catch((err) => console.error(err));
 });
-
-module.exports = router;
+return router;
+};
+module.exports = mentorsRouter;
