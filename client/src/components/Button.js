b6848ca3b75d4ef011269c17471bcc2059a94f8c
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-18 09:57:19 -0500

    Edited Header design

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,11 +1,24 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e9efff",
+    },
+  },
+});
 
-const ButtonUi = (props) => {
-  
-  const {name, bgColor} = props;
-  return <Button variant="contained" style={bgColor}>{name}</Button>;
+const Btn = (props) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button color="primary" variant="contained">
+        {props.children}
+      </Button>
+    </ThemeProvider>
+  );
 };
 
-export default ButtonUi;
+export default Btn;
