ee88180f7f5048bd6714e53ce26b4dd9a974e28b
Alex Thon <ghaiquol@gmail.com>
2022-01-20 21:45:33 -0500

    Add login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import Button from './Button.js';
+import { useState } from 'react';
+import './Login.scss';
+
 const Login = (props) => {
-  console.log(props.btnColor)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState('');
+  
+  const buttonColor = {backgroundColor: '#748FFF'}
+  
+
   return (
+   
     <>
-    <section>login page here </section>
+    <section className="login">
+      <h2 className="title">Login here</h2>
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        
+          <div className="email">
+            <label>Email</label>
+            <input
+              placeholder="Please enter your email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="text"
+            />
+          </div>
+
+          <div className="password">
+           <label>Password</label>
+            <input
+              placeholder="Enter your password here"
+              value={password}
+              onChange={evt => setPassword(evt.target.value)}
+              type="text"
+            />
+          </div>  
+  
+          <Button name='continue' bgColor={buttonColor}></Button>
+      
+      </form>
+    </section>
     </>
     
   
