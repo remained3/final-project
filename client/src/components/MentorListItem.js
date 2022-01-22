f6fe181f989ab92a1352f812a27676f1d14a272c
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-21 21:47:15 -0500

    Display the data into MentorListItem.js

--- client/src/components/MentorListItem.js
+++ client/src/components/MentorListItem.js
@@ -1,24 +1,30 @@
 import React from "react";
-import Button from './Button';
-import {Link} from 'react-router-dom'
+import Button from "./Button";
+import { Link } from "react-router-dom";
 // css
 import "./styles/MentorListItem.scss";
 
 const MentorListItem = (props) => {
-  const {avatar, buttonColor, name, Bio, university, id} = props;
- 
+  const { picture, buttonColor, name, bio, university, id } = props;
+
   return (
-     <section className="mentor-container">
-      <img className="mentor-picture" src={avatar} alt="" />
+    <section className="mentor-container">
+      <img className="mentor-picture" src={picture} alt="" />
       <div className="mentor-details">
         <h4>{name}</h4>
-        <h5><span id="bio">Bio:</span>{Bio}</h5>
-        <h5><span id="uni">University:</span>{university}</h5>
+        <h5>
+          <span id="bio">Bio:</span>
+          {bio}
+        </h5>
+        <h5>
+          <span id="uni">University:</span>
+          {university}
+        </h5>
       </div>
-      <Link to={`mentor/${id}`} >
-        <Button name='Reach out' bgColor={buttonColor}></Button>
+      <Link to={`mentor/${id}`}>
+        <Button name="Reach out" bgColor={buttonColor}></Button>
       </Link>
-     </section>
+    </section>
   );
 };
 
