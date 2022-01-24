2f1afeeb465c4b45f4623333c3b770e5f3bd48ee
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-23 23:31:22 -0500

    Fixed db bug in mentors.js

--- server/routes/mentors.js
+++ server/routes/mentors.js
@@ -1,30 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // GET mentors
 // not using database yet
 const mentorsRouter = (db) => {
-  router.get('/',function(req, res, next) {
+  router.get("/", function (req, res, next) {
     const queryString = `
     SELECT users.id,users.name, users.email, users.password,
           users.picture, users.mentor, users.institution_id,
-          users.bio, users.last_active, institutions.institution
+          users.bio, users.last_active, institutions.name
     FROM users 
     JOIN
-      institutions
+    institutions
     ON
       users.institution_id=institutions.id
     WHERE mentor = true;
-    `
-    ;
-    return (
-          db
-          .query(queryString)
-          .then((data) => res.json(data.rows))
-          .catch((err) => console.error(err))
-    );
-});
-return router;
+    `;
+    return db
+      .query(queryString)
+      .then((data) => res.json(data.rows))
+      .catch((err) => console.error(err));
+  });
+  return router;
 };
 
 module.exports = mentorsRouter;
