715a0a6873278e78a682c6c03dd649ea48b8fa6a
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-25 22:09:35 -0500

    Installed io for the question.js

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from "react";
 import image from "./images/alison.png";
 import "./Question.scss";
 import Button from "./Button";
-
-import {Link, useParams} from 'react-router-dom'
-
-
+import io from "socket.io-client";
+import { Link, useParams } from "react-router-dom";
 
 const Question = (props) => {
-  const buttonColor = {backgroundColor: '#748FFF'};
-  const {users} = props;
+  const buttonColor = { backgroundColor: "#748FFF" };
+  const { users } = props;
 
-  let selectedMentorId = parseInt(useParams().id)
-  const selectedMentor = users.filter(user => user.id === selectedMentorId);
-  
-  const {picture, name, bio, institution,last_active, id} = selectedMentor[0];
-
+  let selectedMentorId = parseInt(useParams().id);
+  const selectedMentor = users.filter((user) => user.id === selectedMentorId);
+  if (!selectedMentor[0]) {
+    return;
+  }
+  const { picture, name, bio, institution, last_active, id } =
+    selectedMentor[0];
 
   return (
     <section className="question-container">
@@ -24,37 +24,31 @@ const Question = (props) => {
       </div>
 
       <div className="details-textContainer">
-          <div className="question-details">
-            <h4 id="name">{name}</h4>
-            <h5>
-              <span id="bio">Bio:</span>
-              {bio}
-            </h5>
-            <h5>
-              <span id="uni">University:</span>
-              {institution}
-            </h5>
-            <h5>
-              <span id="status">status:</span>
-              {last_active}
-            </h5>
-          </div>
+        <div className="question-details">
+          <h4 id="name">{name}</h4>
+          <h5>
+            <span id="bio">Bio:</span>
+            {bio}
+          </h5>
+          <h5>
+            <span id="uni">University:</span>
+            {institution}
+          </h5>
+          <h5>
+            <span id="status">status:</span>
+            {last_active}
+          </h5>
+        </div>
 
-          <div className="text">
-            <textarea
-              className="textarea"
-              placeholder="Write something.."
-              style={{ height: "300px", width: "300px" }}
-              > 
-            </textarea>
-          </div>
-          <Button 
-            className="btn" 
-            name="send" 
-            bgColor={buttonColor} 
-            />
+        <div className="text">
+          <textarea
+            className="textarea"
+            placeholder="Write something.."
+            style={{ height: "300px", width: "300px" }}
+          ></textarea>
+        </div>
+        <Button className="btn" name="send" bgColor={buttonColor} />
       </div>
-     
     </section>
   );
 };
